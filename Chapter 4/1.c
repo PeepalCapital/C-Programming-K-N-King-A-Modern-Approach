44ce75204e75dbf217e952137a89ccd67e6e57ab
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-01-28 11:20:33 -0800

    Update 1.c

--- Chapter 4/1.c
+++ Chapter 4/1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int n, units, tens;
+
+    printf("Enter a two-digit number: ");
+    scanf("%d", &n);
+
+    units = n % 10;
+    tens = n / 10;
+
+    printf("The reversal is: %d%d\n", units, tens);
+
+    return 0;
+}
