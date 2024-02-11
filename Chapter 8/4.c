3427f3db6f8ac4fa8b9563c630c51d28708fc17c
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-02-10 17:32:09 -0800

    Create 4.c

---
+++ Chapter 8/4.c
@@ -0,0 +1,24 @@
+/*
+    The intent of the macro and sizeof is not very clear to me
+*/
+
+#include<stdio.h>
+
+#define SIZE ((int) (sizeof(a) / sizeof(a[0])))
+
+int main(void){
+    int a[10], i;
+
+    printf("Enter %d numbers: ", SIZE);
+    for (i = 0; i < SIZE; i++){
+        scanf("%d", &a[i]);
+    }
+
+    printf("In reverse order: ");
+    for (i = SIZE - 1; i >= 0; i--){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
